9afc3f86ea0fd0449c029bc869983deaf9744e2a
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-05-29 13:30:38 -0400

    Added app resources Implemented horizontal scrollers with animation

---
+++ TouchDown/App/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  TouchDown
+//
+//  Created by M_2195552 on 2023-05-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //MARK: - Properties
+    
+    //MARK: - Body
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(.white)
+                    .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(spacing: 0) {
+                        FeaturedTabView()
+                            .padding(.vertical, 20)
+                            .frame(width: .infinity, height: 280)
+                        
+                        CategoryGridView()
+                        
+                        TitleView(title: "Helmets")
+                        
+                        LazyVGrid(columns: gridLayout, spacing: 15) {
+                            ForEach(products) { product in
+                              ProductItemView(product: product)
+                                .onTapGesture {
+                                  feedback.impactOccurred()
+                                  
+                                  withAnimation(.easeOut) {
+//                                    shop.selectedProduct = product
+//                                    shop.showingProduct = true
+                                  }
+                                }
+                            }
+                        } //: LazyVGrid
+                        .padding(15)
+                        
+                        FooterView()
+                            .padding(.horizontal)
+                    } //: VStack
+                } //: ScrollView
+            } //: VStack
+            .background(colorBackground.ignoresSafeArea())
+        } //: ZStack
+        .ignoresSafeArea()
+    }
+}
+ //MARK: - Preview
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
