876b2ec4827bb373dbadd3635195c9f2ff80fb8d
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-05-29 10:30:02 -0400

    Initial project commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by M_2195552 on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
